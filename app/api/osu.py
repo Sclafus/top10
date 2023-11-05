66fe2174a57bdcd07aa4f2cf8e91c6feac00dc36
Sclafus <alessandro.sclafani00@gmail.com>
2023-11-05 12:08:20 +0100

    api: session token is retrieved automatically

--- app/api/osu.py
+++ app/api/osu.py
@@ -1,10 +1,11 @@
 from requests_oauthlib import OAuth2Session
 from app.common import OAUTH_CONFIG
+from flask import session
 
-def get_osu_user_info(token: str):
+def get_osu_user_info(token: str = None) -> dict:
 	osu = OAuth2Session(
 		OAUTH_CONFIG.client_id,
-		token=token,
+		token=token or session["oauth_token"],
 	)
 	user_info = osu.get("https://osu.ppy.sh/api/v2/me").json()
 	return user_info
