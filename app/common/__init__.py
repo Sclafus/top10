1c135927bb0de5abb596698353202b677fd93d55
Sclafus <alessandro.sclafani00@gmail.com>
2023-11-05 10:20:53 +0100

    common: added db initialization

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,5 +1,7 @@
 from app.config.oauth import OAuthConfig
 from app.config.db import DBConfig
+from app.db.orm import init
 
 OAUTH_CONFIG = OAuthConfig()
 DB_CONFIG = DBConfig()
+db = init(DB_CONFIG)
