df9feddcc0081188a44341a121614b511e0ffc6a
Sclafus <alessandro.sclafani00@gmail.com>
2023-11-05 12:07:58 +0100

    common: moved a line to avoid circular imports

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,7 +1,8 @@
 from app.config.oauth import OAuthConfig
 from app.config.db import DBConfig
-from app.db.orm import init
 
 OAUTH_CONFIG = OAuthConfig()
 DB_CONFIG = DBConfig()
+
+from app.db.orm import init  # noqa: E402
 db = init(DB_CONFIG)
